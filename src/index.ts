877afb86826781c384bbe664506a15a4813c5b34
Paulo Ngimbi <pauloczer@gmail.com>
2024-03-05 12:37:01 +0100

    feat: initial config

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { server } from "./server/Server";
+
+server.listen(3333, () => console.log('server listening on port 3333'));
---
+++ src/server/Server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const server = express();
+
+server.get('/', (_, res) => {
+
+    return res.send('Olá, Dev');
+})
+
+
+export { server} ;
